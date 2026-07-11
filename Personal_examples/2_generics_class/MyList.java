7eeb2d5f03f30867683cb1516a68583745d990a4
Heinz Windzio <hwindzio@gmail.com>
2026-07-11 14:38:38 +0000

    Generic types and Lists and streams

--- Personal_examples/2_generics_class/MyList.java
+++ Personal_examples/2_generics_class/MyList.java
@@ -11,35 +11,27 @@ public class MyList {
         List fruits = new ArrayList();
         fruits.add("kiwi");
         String first = (String)fruits.get(0); // must cast Object to a String
-        System.out.println(first); 
         fruits.add(100);
         int second = (int)fruits.get(1); // must cast Object to an int
-        System.out.println(second); 
+        System.out.println(first + second);
+        fruits.stream().forEach(System.out::println);
 
         // still old way
         List<String> fruits2 = new ArrayList<String>();
 
         // new way
         List<String> fruits3 = new ArrayList<>();
-
-        // 2. Adding elements
         fruits3.add("Apple");
-        fruits.add("Banana");
-        fruits.add("Apple"); // Duplicates allowed
+        fruits3.add("Mango");
+        fruits3.stream().forEach(System.out::println);
 
-        // 3. Positional access
-        String firstFruit = fruits.get(0); // "Apple"
+        // immutable List
+        List<Integer> immutableList = List.of(3, 6, 9, 12);
+        // System.out.println(immutableList);
+        System.out.print(immutableList.get(0));
+        immutableList.stream().forEach(System.out::println);
+        // will throw an UnsupportedOperationException
+        // immutableList.add(15); 
 
-        // 4. Modifying an element
-        fruits.set(1, "Mango"); // Replaces "Banana" with "Mango"
-
-        // 5. Removing elements
-        fruits.remove(0); // Removes element at index 0
-
-        // 6. Checking size and presence
-        int size = fruits.size(); // Returns 2
-        boolean hasMango = fruits.contains("Mango"); // Returns true
-
-        */
     }
 }
