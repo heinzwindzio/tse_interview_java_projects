c60339676216c6111b239f751dfb57cf12dec685
Heinz Windzio <hwindzio@gmail.com>
2026-07-12 23:28:44 +0000

    forEach(Consumer Functional Interface)

--- Personal_examples/6_method_reference/FourKinds.java
+++ Personal_examples/6_method_reference/FourKinds.java
@@ -8,13 +8,18 @@ public class FourKinds{
         
         // create a mutable List
         List<String> cars = new ArrayList<>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
+        cars.add("1");
+        cars.add("2");
+        cars.add("3");
 
         // now print it using the List.forEach(Consumer Functional Interface) method
+        // you can't change (add or remove) items in the List...but you can mutate them
         System.out.println("method 1 - put arg into a static method");
-        cars.forEach(str -> String.toString(str));
+        cars.forEach(str -> {
+            str.setName(str.getName() + ":number");
+        });
+        System.out.println("now with method reference");
+        cars.forEach(System.out::println);
         
         
         //cars.forEach(System.out::println);
