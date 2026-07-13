f6b2408ff6a27de6264e1511f724ade6b18e0aed
Heinz Windzio <hwindzio@gmail.com>
2026-07-13 11:33:07 +0000

    reflection

--- Personal_examples/reflection/ClassReflection.java
+++ Personal_examples/reflection/ClassReflection.java
@@ -17,25 +17,26 @@ public class ClassReflection
 
     public static void main(String[] args){
 
-
+        System.out.println("This is a simple toString() call to String: ");
+        System.out.println(s.toString());
 
         // before generics, casting a String to an Integer would throw a runtime error:
         try {
             Class s = String.class;
-            System.out.println(s.getClass());
+            System.out.println("This is the String class: " +s.getClass());
             
- /** 
+            System.out.println("These are the methods of String: ");
             for (Method m : s.getDeclaredMethods()){
                 System.out.println(m.getName());
             }
-*/
 
+            System.out.println("Thses are the fields of String: ");
             Field[] fs = s.getDeclaredFields();
             for (Field f : fs){
                 System.out.println(f);
             }
             
-            s.toString();
+
         }
         catch (Exception e) {
             System.out.println(e);
