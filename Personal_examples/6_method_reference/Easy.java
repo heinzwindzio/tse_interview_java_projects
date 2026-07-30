e201cb5f97057baadefcf86ad6fc416e61f74bdd
Heinz Windzio <hwindzio@gmail.com>
2026-07-30 13:26:35 +0000

    method reference

--- Personal_examples/6_method_reference/Easy.java
+++ Personal_examples/6_method_reference/Easy.java
@@ -6,13 +6,20 @@ public class Easy {
         
         // 1. Consumer - Takes one argument and returns no result (void)
         // Consumer<InputType>
-        // Consumer<String> printer = s -> System.out.println("Printing: " + s);
-        Consumer<String> printer = Easy::printWithPrefix;
-        printer.accept("Suffix, ");
+
+        // Define the one abstract method of the functional interface using a lambda expression
+        Consumer<String> printer1 = s1 -> System.out.println("In Lambda Method Logic. " + s1);
+        printer1.accept("This is the String arg (s1) which passed to the interface's method defined in the lambda expression");
+
+        // Define the one abstract method of the functional interface using a method reference
+        Consumer<String> printer2 = Easy::printWithPrefix;
+        printer2.accept("This is the String arg (s2) passed to the interface's method, which is defined below.");
+
+        printWithPrefix("Just calling the method directly. ");
     }
    
-    public static void printWithPrefix(String s){
-            System.out.println("Prefix, " + s);
+    public static void printWithPrefix(String s2){
+            System.out.println("In Defined Method Logic. " + s2);
     }
     
 }
