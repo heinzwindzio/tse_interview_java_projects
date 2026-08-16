52f4b782182545d0f7f237a2df0e0947003f4740
Heinz Windzio <hwindzio@gmail.com>
2026-08-16 21:41:07 +0000

    correct LongestCommonPrefix2

--- LeetCode/Easy/LongestCommonPrefix2.java
+++ LeetCode/Easy/LongestCommonPrefix2.java
@@ -8,35 +8,62 @@ public class LongestCommonPrefix2 {
 
     public static void main(String[] args){
 
-        String[] words = {"flower","flow","flight"};
+        String[] strs = {"abab","aba",""};
 
-        // the maximum letters that all words have in common
-        int maxLetters = 0;
+        System.out.println("These are the words: ");
+        for (String word : strs) System.out.println(word);
 
-        String firstWord = words[0];
+        String prefix = strs[0]; // assume the first word is the prefix which we compare to
         String nextWord = "";
+        int index = 0;
 
-        // compare prefix against the rest of the words, subtracting from the max length as we go
-        
-        // loop through the array, starting with the second word
-        for (int x=1; x<words.length; x++){
+        // handle case where only 1 word in the array - don't loop
+        if (strs.length == 1){ 
+            prefix = strs[0];
+        }
+        else{ // case where there is more than one word in the array
 
-            // grab the next word
-            nextWord = words[x];
+            outerloop:
+            // loop through the array, starting with the second word
+            for (int x=1; x<strs.length; x++){
 
-            // loop through each letter in the current word
-            for (int y=0; y <nextWord.length(); y++){
+                nextWord = strs[x];
 
-                // compare each letter in the current word with the next word, starting with the char at max length
-                if (firstWord.charAt(y) == nextWord.charAt(y)){
-
-                    // if a match then increase the max length by 1
-                    maxLetters++;
+                // handle case where either of the lengths are zero, then break out of loops
+                if ((prefix.length() == 0) || (nextWord.length() == 0)){
+                    System.out.println("one of the 2 words are empty strings");
+                    prefix = "";
+                    break outerloop;
                 }
-                else break;
-            }   
+
+                // loop through each letter in the shortest word
+                for (int y=0; y<prefix.length() & y<nextWord.length(); y++){
+
+                    // handle a mismatch
+                    if (prefix.charAt(y) != nextWord.charAt(y)){ 
+                        System.out.println("there is a mismatch");
+
+                        // handle where no chars match
+                        if (y==0){
+                            System.out.println("No common prefix letters found. Break out with no value");
+                            prefix = "";
+                            break outerloop;
+                        }
+                        else{ // handle a mismatch within the two strings
+                            System.out.println("We found a mismatch at position: " + y);
+                            prefix = prefix.substring(0, y); // set prefix up to that point
+                            break; // break but keep comparing against other words in the array
+                        }
+                    }
+                    // grab the index
+                    index = y;
+                }   
+                // handle case where second word is shorter
+                prefix = prefix.substring(0, index+1); 
+
+            }
         }
 
-
+        System.out.println("This is prefix: " + prefix);
     }
 }
