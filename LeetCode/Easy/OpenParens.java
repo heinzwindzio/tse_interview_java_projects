92c06c8e6a30d0883b1d41d824cc57c4ba4d7d0a
Heinz Windzio <hwindzio@gmail.com>
2026-08-17 16:11:41 +0000

    OpenParens

---
+++ LeetCode/Easy/OpenParens.java
@@ -0,0 +1,18 @@
+public class OpenParens {
+    
+    public static void main(String[] args){
+
+        String[] parens = {"((()()))"};
+        // String[] parans = {"( { ) }"};
+
+        
+        // count the number of open of a specific type
+        // count the number of closed of a specific type
+        // check to ensure every open comes before a close, based on countOpen and countClose. Use linked list to add and remove
+
+        // hand mixed parens
+        // when opening a parens, ensure that the following parens of a different type are all closed before closing 
+
+
+    }
+}
