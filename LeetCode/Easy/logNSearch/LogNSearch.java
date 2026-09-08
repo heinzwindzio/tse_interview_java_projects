312c868e548b5ba31a6568f1797749dc4e78895c
Heinz Windzio <hwindzio@gmail.com>
2026-09-08 14:51:48 +0000

    LogNSearch final

--- LeetCode/Easy/logNSearch/LogNSearch.java
+++ LeetCode/Easy/logNSearch/LogNSearch.java
@@ -5,7 +5,7 @@ public class LogNSearch {
     public static void main(String[] args){
 
         int[] nums = {1,3,5,6};
-        int target = 4;
+        int target = 7;
         int result;
 
         LogNSearch lns = new LogNSearch();
@@ -35,21 +35,35 @@ public class LogNSearch {
 
             // if the target is larger, then the low gets adjusted to the mid point unless number not found
             if (target > nums[mid]){
+                
+                // if position is at the last index, return index +1
+                if (nums[mid] == nums[nums.length-1]){
+                    return mid+1;
+                }
+                else if (target < nums[mid+1]){
+                    return mid+1;
+                }
                 low = mid+1;
             }
             
             if (target < nums[mid]){
+
+                // if position is at the first index, return index
+                if (mid == 0){
+                    return mid;
+                }
+                else if (target > nums[mid-1]){
+                    return mid;
+                }
                 high = mid-1;
             }
 
+            System.out.println("high: " + high + " low: " + low + " mid: " + mid);
+
         } // while
 
-        // didn't find a number, so return the position it should be in
-        if (target > mid){
-            return mid+1;
-        }
-        else{
-            return mid;
-        }
+        // error
+        return -1;
+
     }
 }
