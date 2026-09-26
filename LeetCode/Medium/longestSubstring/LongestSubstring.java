49dfa3b23eca8f5733b082e20671a3f278a4d322
Heinz Windzio <hwindzio@gmail.com>
2026-09-26 15:17:42 +0000

    LongestSubstring

--- LeetCode/Medium/longestSubstring/LongestSubstring.java
+++ LeetCode/Medium/longestSubstring/LongestSubstring.java
@@ -3,7 +3,7 @@ public class LongestSubstring {
 
     public static void main(String[] args){
 
-        String s = "bbbbb";
+        String s = "abcabcbb";
         LongestSubstring ls = new LongestSubstring();
         System.out.println("This is the longest substring: " +ls.lengthOfLongestSubstring(s));
     }
@@ -16,21 +16,24 @@ public class LongestSubstring {
         for(int x = 0; x<s.length(); x++){
 
             int count = 1;
+            int firstIndex = 0; // position of the first index of the duplicate char
+            String compare = Character.toString(s.charAt(x));
 
-            // put the first char in a String of unique chars
-            String unique = Character.toString(s.charAt(x));
+            // create a new substring to search, without the compare char
+            String temp = s.substring(x+1);
+            System.out.println(temp);
 
-            // create a new substring to search, starting with the current char
-            String temp = s.substring(x);
+            // search the temp String for the first index of the duplicate char
+            firstIndex = temp.indexOf(compare);
 
-            // search the temp String for the char
-            // if not there, increment the count and put the char in the String
-            if( !unique.contains(Character.toString(temp.charAt(y))) ){
-                count++;
-                unique = unique + s.charAt(y);
+            // if it exists, then count = index - 1
+            if(firstIndex != -1){
+                System.out.println(firstIndex); 
+                count = firstIndex+1;
             }
-            else break; // otherwise break out of this loop
-
+            else{ // otherwise the count = temp.length()
+                count = temp.length()+1;
+            }
 
             // if the count is bigger than the result, then result = count
             if(count>result) result = count;
--- LeetCode/Medium/longestSubstring/LongestSubstringRejected.java
+++ LeetCode/Medium/longestSubstring/LongestSubstringRejected.java
@@ -1,4 +1,4 @@
-public class LongestSubstring {
+public class LongestSubstringRejected {
     
 
     public static void main(String[] args){
